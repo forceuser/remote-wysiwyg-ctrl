aecd7b887100082c68927d4f383e99b670952ee1
Vitaly Dub <forceuserz@gmail.com>
2018-02-27 13:03:40 +0200

    redy

---
+++ index.js
@@ -0,0 +1,39 @@
+
+export default function open (url, data) {
+	return new Promise((resolve, reject) => {
+		const ident = btoa(Math.random()).replace(/\=/ig, "");
+		window.open(`?init=${ident}`, ident);
+		window.addEventListener("message", event => {
+			if (event.data) {
+				const data = JSON.parse(event.data);
+				const {type, id} = data;
+
+				if (id === ident) {
+					switch (type) {
+						case "init":
+						{
+
+							event.source.postMessage(JSON.stringify({
+								type: "init",
+								id,
+								data: Object.assign({}, data, {callbackId: id}),
+							}), "*");
+							break;
+						}
+						case "save":
+						{
+							resolve(data.content);
+							event.source.close();
+							break;
+						}
+						case "cancel":
+						{
+							reject("cancel");
+							break;
+						}
+					}
+				}
+			}
+		});
+	});
+}
